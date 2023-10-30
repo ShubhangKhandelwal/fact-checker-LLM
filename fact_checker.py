c4ced132d69cec904cd8a63837e088aed2b3a71c
ShubhangKhandelwal <83733693+ShubhangKhandelwal@users.noreply.github.com>
2023-10-30 13:27:14 +0530

    Update fact_checker.py

--- fact_checker.py
+++ fact_checker.py
@@ -3,41 +3,40 @@ from langchain.chains import LLMChain, SimpleSequentialChain
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 
-st.title("What's True: using Langchain SimpleSequentialChain")
-st.markdown("ask anything you have in mind!!")
+def main():
+    st.title("What's True: using Langchain SimpleSequentialChain")
+    st.markdown("Ask anything you have in mind!!")
 
-# API = YOUR_API_KEY 
-if API:
+    API = st.text_input("Enter your OpenAI API-Key:", type="password")
+    
+    if not API:
+        st.warning("Enter your OpenAI API-Key. Get your key from [here](https://platform.openai.com/account/api-keys).\n")
+        return
+
     llm = OpenAI(temperature=0.8, openai_api_key=API)
-else:
-    st.warning("Enter your OpenAI API-Key. Get your key from [here](https://platform.openai.com/account/api-keys).\n")
 
-user_question = st.text_input("Enter your question:",
-                              placeholder="Cyanobacteria can perform photosynthesis, are they considered as plants?")
+    user_question = st.text_input(
+        "Enter your question:",
+        placeholder="Cyanobacteria can perform photosynthesis, are they considered as plants?"
+    )
 
-if st.button("seach", type="primary"):
-    template = "{question}\n\n"
-    prompt_template = PromptTemplate(input_variables=["question"], template=template)
-    question_chain = LLMChain(llm=llm, prompt=prompt_template)
+    if st.button("Search", type="primary"):
+        run_langchain(user_question, llm)
 
-    template = """
-    {statement}
-    Make points that would be used to give solution for it.\n\n"""
-    prompt_template = PromptTemplate(input_variables=['statement'], template=template)
-    assumption_chain = LLMChain(llm=llm, prompt=prompt_template)
-    assumption_chain_seq = SimpleSequentialChain(chains=[question_chain, assumption_chain], verbose=True)
+def run_langchain(user_question, llm):
+    question_chain = create_llm_chain(llm, "question", "{question}\n\n")
+    assumption_chain = create_llm_chain(llm, "statement", "{statement}\nMake points that would be used to give a solution for it.\n\n")
+    fact_checker_chain = create_llm_chain(llm, "assertions", "Here is a bullet point list of assertions:\n{assertions}\nConsider it and make a solution out of it\n\n")
+    answer_chain = create_llm_chain(llm, "facts", "{facts}\nIn light of the above solution, give a perfect and simple solution'{user_question}'")
 
-    template = """Here is a bullet point list of assertions:
-    {assertions}
-    consider it and make solution out of it\n\n"""
-    prompt_template = PromptTemplate(input_variables=["assertions"], template=template)
-    fact_checker_chain = LLMChain(llm=llm, prompt=prompt_template)
-    fact_checker_chain_seq = SimpleSequentialChain(chains=[question_chain, assumption_chain, fact_checker_chain], verbose=True)
-
-    template = """In light of the above solution, give a perfect and simple solution'{}'""".format(user_question)
-    template = """{facts}\n""" + template
-    prompt_template = PromptTemplate(input_variables=["facts"], template=template)
-    answer_chain = LLMChain(llm=llm, prompt=prompt_template)
-    overall_chain = SimpleSequentialChain(chains=[question_chain, assumption_chain, fact_checker_chain, answer_chain], verbose=True)
+    chains = [question_chain, assumption_chain, fact_checker_chain, answer_chain]
+    overall_chain = SimpleSequentialChain(chains=chains, verbose=True)
 
     st.success(overall_chain.run(user_question))
+
+def create_llm_chain(llm, input_variable, template):
+    prompt_template = PromptTemplate(input_variables=[input_variable], template=template)
+    return LLMChain(llm=llm, prompt=prompt_template)
+
+if __name__ == "__main__":
+    main()
